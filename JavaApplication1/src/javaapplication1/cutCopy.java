6c678348d7c768fa1c4b79e4f0f96cf0a3f4efe0
dhanur350 <dhanur297@gmail.com>
2022-03-30 11:36:45 +0530

    Day7.1

--- JavaApplication1/src/javaapplication1/cutCopy.java
+++ JavaApplication1/src/javaapplication1/cutCopy.java
@@ -7,28 +7,21 @@ package javaapplication1;
 import java.awt.*;
 import java.awt.event.*;
 
-public class cutCopy extends Frame {
+public class cutCopy extends Frame implements ActionListener {
 
     TextArea textArea;
     Button cutButton, copyButton, pasteButton;
+    public String s1;
 
-    public cutCopy() {
+    cutCopy() {//constructor
         setTitle("Sample text area for testing cut copy paste");
         setLayout(new FlowLayout());
         textArea = new TextArea(5, 20);
         cutButton = new Button("Cut");
         pasteButton = new Button("Paste");
         copyButton = new Button("Copy");
-        cutButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                if (ae.getSource() == cutButton) {
-                    //;
-                } else if (ae.getSource() == copyButton) {
-                    //c = a - b;
-                } else if (ae.getSource() == pasteButton) {
-                }
-            }
-        });
+        cutButton.addActionListener(this);
+        pasteButton.addActionListener(this);
         add(textArea);
         add(cutButton);
         add(copyButton);
@@ -36,6 +29,18 @@ public class cutCopy extends Frame {
         setSize(500, 250);
         setLocationRelativeTo(null);
         setVisible(true);
+//public void run(){}
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == cutButton) {
+            s1 = textArea.getSelectedText();//copy to clipboard
+        } else if (ae.getSource() == copyButton) {
+        } else if (ae.getSource() == pasteButton) {
+            if (textArea != null) {
+                textArea.append(s1);//append it to last text written
+            }
+        }
     }
 
     public static void main(String args[]) {
