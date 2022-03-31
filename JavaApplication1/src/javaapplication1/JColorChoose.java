92cf5100b2fedb932fd9c87595a0104b24ab53e4
dhanur350 <dhanur297@gmail.com>
2022-03-31 11:56:34 +0530

    Day8.1

---
+++ JavaApplication1/src/javaapplication1/JColorChoose.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class JColorChoose extends JFrame implements ActionListener {
+
+    JButton b;
+    Container c;
+
+    JColorChoose() {
+        c = getContentPane();
+        c.setLayout(new FlowLayout());
+        b = new JButton("color");
+        b.addActionListener(this);
+        c.add(b);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Color initialcolor = Color.RED;
+        Color color = JColorChooser.showDialog(this, "Select a color", initialcolor);
+        c.setBackground(color);
+    }
+
+    public static void main(String[] args) {
+        JColorChoose ch = new JColorChoose();
+        ch.setSize(400, 400);
+        ch.setVisible(true);
+        ch.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
