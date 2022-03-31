856b9f1a6cc54f4714d053fb04719dff877b3292
dhanur350 <dhanur297@gmail.com>
2022-03-31 11:41:50 +0530

    Day8

---
+++ JavaApplication1/src/javaapplication1/JButtonEvent.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JButtonEvent {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Button Example");
+        final JTextField tf = new JTextField();
+        tf.setBounds(50, 50, 150, 20);
+        JButton b = new JButton("Click Here");
+        b.setBounds(50, 100, 95, 30);
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("Welcome to Javatpoint.");
+            }
+        });
+        f.add(b);
+        f.add(tf);
+        f.setSize(400, 400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
---
+++ JavaApplication1/src/javaapplication1/TicTacToe.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+ * Created by JFormDesigner on Thu Dec 08 14:16:42 EST 2011
+ */
+/**
+ * @author Bradley Chelsey
+ */
+public class TicTacToe extends Frame implements ActionListener {
+
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
+
+    TicTacToe() {
+        setLayout(new FlowLayout());
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+}
